aebb64889ec2ce643b24eb70804ef88525cb78ce
Hamza Hassan <hamzahassan@Hamzas-MacBook-Pro.local>
2021-07-14 23:27:42 -0700

    finish ex2 with ec

--- src/__tests__/exercise/02.js
+++ src/__tests__/exercise/02.js
@@ -6,35 +6,18 @@ import ReactDOM from 'react-dom'
 // 🐨 import the `render` and `fireEvent` utilities from '@testing-library/react'
 import {render, fireEvent} from '@testing-library/react'
 import Counter from '../../components/counter'
-
-// 💣 remove this. React Testing Library does this automatically!
-beforeEach(() => {
-  document.body.innerHTML = ''
-})
+import {toHaveTextContent} from '@testing-library/jest-dom'
 
 test('counter increments and decrements when the buttons are clicked', () => {
-  // 💣 remove these two lines, React Testing Library will create the div for you
-
-  // 🐨 swap ReactDOM.render with React Testing Library's render
-  // Note that React Testing Library's render doesn't need you to pass a `div`
-  // so you only need to pass one argument. render returns an object with a
-  // bunch of utilities on it. For now, let's just grab `container` which is
-  // the div that React Testing Library creates for us.
-
+  //arrange..
   const {container} = render(<Counter />)
-
-  // 🐨 instead of `div` here you'll want to use the `container` you get back
-  // from React Testing Library
   const [decrement, increment] = container.querySelectorAll('button')
   const message = container.firstChild.querySelector('div')
-
   expect(message.textContent).toBe('Current count: 0')
 
-  // 🐨 replace the next two statements with `fireEvent.click(button)`
+  fireEvent.click(increment) //act!
+  expect(message).toHaveTextContent('Current count: 1') //assert!
 
-  fireEvent.click(increment)
-  expect(message.textContent).toBe('Current count: 1')
-
-  fireEvent.click(decrement)
-  expect(message.textContent).toBe('Current count: 0')
+  fireEvent.click(decrement) //act!
+  expect(message).toHaveTextContent('Current count: 0') //assert!
 })
